63b832dade5c5cc8e8970da08422abd3584a7037
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-14 11:39:46 +0430

    add Exceptions.kt

---
+++ app/src/main/java/ir/ah/app/bumgard/other/util/Exceptions.kt
@@ -0,0 +1,8 @@
+package ir.ah.app.bumgard.other.util
+
+import java.text.*
+import java.util.*
+
+import java.io.IOException
+
+class NoInternetException(message: String? = null) : IOException(message)
