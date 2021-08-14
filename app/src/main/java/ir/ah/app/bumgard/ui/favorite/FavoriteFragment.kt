1288e9de1151eb11e45a2abb5453dfa8822acf30
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-14 11:55:28 +0430

    add FavoriteFragment.kt

---
+++ app/src/main/java/ir/ah/app/bumgard/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,28 @@
+package ir.ah.app.bumgard.ui.favorite
+
+
+import dagger.hilt.android.*
+import ir.ah.app.bumgard.R
+import ir.ah.app.bumgard.base.*
+import ir.ah.app.bumgard.databinding.*
+import ir.ah.app.bumgard.other.*
+
+@AndroidEntryPoint
+class FavoriteFragment : BaseFragment<FavoriteViewModel>(
+    R.layout.fragment_favorite, FavoriteViewModel::class
+) {
+    private val binding by viewBinding(FragmentSearchBinding::bind)
+
+
+    override fun observeData() {
+        super.observeData()
+
+    }
+
+    override fun setUpViews() {
+        super.setUpViews()
+
+    }
+
+
+}
