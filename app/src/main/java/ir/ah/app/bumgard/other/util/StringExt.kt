d9c998d4453f529f8d30759d37882237dc5cda90
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-16 15:15:22 +0430

    add isValidEmail function

--- app/src/main/java/ir/ah/app/bumgard/other/util/StringExt.kt
+++ app/src/main/java/ir/ah/app/bumgard/other/util/StringExt.kt
@@ -7,3 +7,6 @@ fun dateToName(date: Date): String {
     val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
     return dateFormat.format(date.toLocaleString())
 }
+
+fun String.isValidEmail() =
+    isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
