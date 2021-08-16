f6ba71dec6f14ecfa9fddf3cf96c098e37e967de
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-16 15:20:36 +0430

    add check log in user

--- app/src/main/java/ir/ah/app/bumgard/ui/splash/SplashFragment.kt
+++ app/src/main/java/ir/ah/app/bumgard/ui/splash/SplashFragment.kt
@@ -9,8 +9,12 @@ import ir.ah.app.bumgard.R
 import ir.ah.app.bumgard.base.*
 import ir.ah.app.bumgard.databinding.*
 import ir.ah.app.bumgard.other.*
+import ir.ah.app.bumgard.other.util.*
+import ir.ah.app.bumgard.other.util.Constance.RESULT_LOGIN
+import ir.ah.app.bumgard.other.wrapper.*
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import javax.inject.*
 @AndroidEntryPoint
 class SplashFragment : BaseFragment<SplashViewModel>(
@@ -21,15 +25,42 @@ class SplashFragment : BaseFragment<SplashViewModel>(
 
     override fun observeData() {
         super.observeData()
-        lifecycleScope.launch {
+        lifecycleScope.launchWhenStarted {
             delay(2000)
-        findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToAuth())
+            vm.splashEvent.collectLatest { event ->
+                handleResource(event, vm::refreshToken)
+                when (event) {
+                    is Resource.Failure -> {
+
+                    }
+                    Resource.Loading -> {
+
+                    }
+                    is Resource.Success -> {
+                        if (event.success) {
+                            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToAuth())
+                        } else {
+                            findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToSearchFragment())
+                        }
+                    }
+                }
+            }
         }
     }
-
+    private fun getLoginNavigationResult() {
+        getNavigationResult<Boolean>(
+            RESULT_LOGIN,
+            findNavController()
+        )?.observe(viewLifecycleOwner) {
+            if (it) {
+                findNavController().navigate(SplashFragmentDirections.actionSplashFragmentToSearchFragment())
+                mainActivity?.showBottomNav()
+            }
+        }
+    }
     override fun setUpViews() {
         super.setUpViews()
-
+        getLoginNavigationResult()
     }
 
 
