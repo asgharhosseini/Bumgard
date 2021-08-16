df96e3a307fc6b118eb5b8b77e7141bd05d7f41f
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-16 15:14:38 +0430

    add cons value in Constance.kt

--- app/src/main/java/ir/ah/app/bumgard/other/util/Constance.kt
+++ app/src/main/java/ir/ah/app/bumgard/other/util/Constance.kt
@@ -8,4 +8,10 @@ object Constance {
     const val NOTIFICATION_CHANNEL_ID = "bumgard_channel"
     const val NOTIFICATION_CHANNEL_NAME = "bumgard"
     const val NOTIFICATION_ID = 1
+
+    const val SHARED_PREFERENCES_NAME = "userInfo"
+    const val REQUEST_CODE_LOCATION_PERMISSION = 0
+
+    const val RESULT_LOGIN = "result_login"
+    const val RESULT_REGISTER = "result_register"
 }
