d9ecb30f62fa1d25a6d79f2b36a53ed70a52fc24
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-16 15:19:50 +0430

    add refreshToken in SplashViewModel.kt

--- app/src/main/java/ir/ah/app/bumgard/ui/splash/SplashViewModel.kt
+++ app/src/main/java/ir/ah/app/bumgard/ui/splash/SplashViewModel.kt
@@ -2,15 +2,35 @@ package ir.ah.app.bumgard.ui.splash
 
 import dagger.hilt.android.lifecycle.*
 import ir.ah.app.bumgard.base.*
+import ir.ah.app.bumgard.data.local.sharedpreferences.*
+import ir.ah.app.bumgard.other.wrapper.*
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
 import kotlinx.coroutines.flow.*
 import javax.inject.*
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
     val mainCoroutineDispatcher: CoroutineDispatcher,
+    private val userInfoManager: UserInfoManager
 ) : BaseViewModel(mainCoroutineDispatcher) {
+
+    fun refreshToken() = doInMain {
+        if (userInfoManager.getEmail() != "" &&
+            userInfoManager.getToken() != ""
+
+        ) {
+            splashEventChannel.send(Resource.Success(true))
+        }
+    }
+
+    private val splashEventChannel = Channel<Resource<Boolean>>()
+    val splashEvent = splashEventChannel.receiveAsFlow()
+
+    init {
+        refreshToken()
+    }
 }
 
 
