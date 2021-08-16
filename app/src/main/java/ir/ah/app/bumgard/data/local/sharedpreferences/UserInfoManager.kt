6787adf9bf931a79a3efbdfee5e518a63f763974
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-16 15:13:57 +0430

    add UserInfoManager.kt

---
+++ app/src/main/java/ir/ah/app/bumgard/data/local/sharedpreferences/UserInfoManager.kt
@@ -0,0 +1,30 @@
+package ir.ah.app.bumgard.data.local.sharedpreferences
+
+import android.content.*
+import javax.inject.*
+
+
+class UserInfoManager @Inject constructor(private val sharedPreferences: SharedPreferences) {
+
+
+    fun saveUser(email: String, token: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString("email", email)
+        editor.putString("token", token)
+        editor.apply()
+    }
+
+
+
+    fun getToken(): String? {
+        return sharedPreferences.getString("token", null)
+    }
+
+    fun getEmail(): String? {
+        return sharedPreferences.getString("email", null)
+    }
+
+    fun clear() = sharedPreferences.edit().clear().apply()
+
+
+}
