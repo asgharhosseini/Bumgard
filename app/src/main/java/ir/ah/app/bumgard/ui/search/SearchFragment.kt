6dbeace99ac43c128483a112ab31763b7fce94fc
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-17 14:06:08 +0430

    fix ui bug  and add animation to ui

--- app/src/main/java/ir/ah/app/bumgard/ui/search/SearchFragment.kt
+++ app/src/main/java/ir/ah/app/bumgard/ui/search/SearchFragment.kt
@@ -1,12 +1,17 @@
 package ir.ah.app.bumgard.ui.search
 
 
+import android.view.*
+import android.view.animation.*
 import dagger.hilt.android.*
 import ir.ah.app.bumgard.*
 import ir.ah.app.bumgard.R
 import ir.ah.app.bumgard.base.*
 import ir.ah.app.bumgard.databinding.*
 import ir.ah.app.bumgard.other.*
+import ir.ah.app.bumgard.other.util.*
+import ir.ah.app.bumgard.other.util.UtilityAnimation.fadeVisibility
+
 
 @AndroidEntryPoint
 class SearchFragment : BaseFragment<SearchViewModel>(
@@ -23,8 +28,26 @@ class SearchFragment : BaseFragment<SearchViewModel>(
     override fun setUpViews() {
         super.setUpViews()
         (activity as MainActivity).showBottomNav()
+        handleSearchEditor()
+
 
     }
+    private fun handleSearchEditor(){
+        binding.edtSearch.afterTextChanged { it ->
+            if (it.length > 0) {
+                binding.filterLayout.fadeVisibility(View.VISIBLE, 600)
+                binding.baseView.fadeVisibility(View.GONE, 600)
+                binding.searchResultView.fadeVisibility(View.VISIBLE, 600)
 
+
+            } else {
+                binding.filterLayout.fadeVisibility(View.GONE, 600)
+                binding.baseView.fadeVisibility(View.VISIBLE, 600)
+                binding.searchResultView.fadeVisibility(View.GONE, 600)
+
+            }
+
+        }
+    }
 
 }
