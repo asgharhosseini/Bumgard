5d10fbc3c3a1aa55fe06a8f4f810215359bf08f8
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-18 12:19:50 +0430

    fix bug url image

--- app/src/main/java/ir/ah/app/bumgard/data/repository/search/factory/DummyFactory.kt
+++ app/src/main/java/ir/ah/app/bumgard/data/repository/search/factory/DummyFactory.kt
@@ -22,7 +22,9 @@ object DummyFactory {
     fun cityListGenerator():List<City>{
         val cityList:ArrayList<City> = arrayListOf()
         for (i in 0..10){
-            val city =City(i,i,"London","",55.5,55.5, hotelListGenerator())
+            val city =City(i,i,"London $i",
+                "http://bmtehran.ir/wp-content/uploads/2018/08/50-%D8%B1%D8%B3%D8%AA%D9%88%D8%B1%D8%A7%D9%86-%D8%A8%D8%B1%D8%AA%D8%B1-%D8%A7%DB%8C%D8%B1%D8%A7%D9%86-%D8%A8%D9%87%D8%AA%D8%B1%DB%8C%D9%86-%D8%B1%D8%B3%D8%AA%D9%88.jpg",
+                55.5,55.5, hotelListGenerator())
             cityList.add(city)
         }
         return cityList
