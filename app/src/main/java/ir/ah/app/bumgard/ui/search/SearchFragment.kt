70ea7438342bb0f8da82043c3742b4eac0aa44ba
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-18 12:22:55 +0430

    get data and show in ui

--- app/src/main/java/ir/ah/app/bumgard/ui/search/SearchFragment.kt
+++ app/src/main/java/ir/ah/app/bumgard/ui/search/SearchFragment.kt
@@ -1,8 +1,10 @@
 package ir.ah.app.bumgard.ui.search
 
 
+import android.util.*
 import android.view.*
-import android.view.animation.*
+import androidx.lifecycle.*
+import androidx.recyclerview.widget.*
 import dagger.hilt.android.*
 import ir.ah.app.bumgard.*
 import ir.ah.app.bumgard.R
@@ -10,7 +12,12 @@ import ir.ah.app.bumgard.base.*
 import ir.ah.app.bumgard.databinding.*
 import ir.ah.app.bumgard.other.*
 import ir.ah.app.bumgard.other.util.*
+import ir.ah.app.bumgard.other.util.Constance.TAG
 import ir.ah.app.bumgard.other.util.UtilityAnimation.fadeVisibility
+import ir.ah.app.bumgard.other.wrapper.*
+import ir.ah.app.bumgard.ui.search.adapter.*
+import kotlinx.coroutines.flow.*
+import javax.inject.*
 
 
 @AndroidEntryPoint
@@ -18,20 +25,74 @@ class SearchFragment : BaseFragment<SearchViewModel>(
     R.layout.fragment_search, SearchViewModel::class
 ) {
     private val binding by viewBinding(FragmentSearchBinding::bind)
-
+    @Inject
+    lateinit var topCityAdapter: TopCityAdapter
+    @Inject
+    lateinit var popularCityAdapter: PopularCityAdapter
 
     override fun observeData() {
         super.observeData()
-
+        subscribeToObserve()
     }
 
     override fun setUpViews() {
         super.setUpViews()
         (activity as MainActivity).showBottomNav()
         handleSearchEditor()
+        setUpAdapter()
+
 
 
     }
+
+    private fun subscribeToObserve() {
+        vm.getTopCity()
+        vm.getPopularCity()
+        lifecycleScope.launchWhenStarted {
+            vm.topCity.collectLatest { event ->
+                handleResource(event) { vm.getTopCity() }
+                when(event){
+                   is Resource.Loading ->{}
+                   is Resource.Success ->{
+                       topCityAdapter.submitList(event.success.cities)
+                   }
+                   is Resource.Failure ->{}
+                }
+                vm.popularCity.collectLatest { event ->
+                    handleResource(event) { vm.getPopularCity() }
+                    when(event){
+                        is Resource.Loading ->{}
+                        is Resource.Success ->{
+                            popularCityAdapter.submitList(event.success.cities)
+                            Log.e(TAG,event.success.cities[0].name)
+                        }
+                        is Resource.Failure ->{}
+                    }
+
+            }
+
+            }
+
+        }
+
+
+    }
+
+    private fun setUpAdapter() {
+        topCityAdapter.setOnItemClickListener { }
+        binding.rvSearchFragmentTop.apply {
+            adapter = topCityAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
+        popularCityAdapter.setOnItemClickListener { }
+        binding.rvSearchFragmentPopular.apply {
+            adapter = popularCityAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
+    }
+
     private fun handleSearchEditor(){
         binding.edtSearch.afterTextChanged { it ->
             if (it.length > 0) {
