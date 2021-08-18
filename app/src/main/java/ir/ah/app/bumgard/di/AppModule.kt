8b1f1769da8c91ccfd503ab330e500da087d059f
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-18 12:20:09 +0430

    fix bug in glide

--- app/src/main/java/ir/ah/app/bumgard/di/AppModule.kt
+++ app/src/main/java/ir/ah/app/bumgard/di/AppModule.kt
@@ -2,6 +2,7 @@ package ir.ah.app.bumgard.di
 
 import android.content.*
 import com.bumptech.glide.*
+
 import com.bumptech.glide.load.engine.*
 import com.bumptech.glide.load.resource.bitmap.*
 import com.bumptech.glide.request.*
@@ -9,6 +10,8 @@ import dagger.*
 import dagger.hilt.*
 import dagger.hilt.android.qualifiers.*
 import dagger.hilt.components.*
+import ir.ah.app.bumgard.*
+import ir.ah.app.bumgard.R
 import javax.inject.*
 
 @Module
@@ -20,8 +23,8 @@ object AppModule {
         @ApplicationContext context: Context
     ) = Glide.with(context).setDefaultRequestOptions(
         RequestOptions()
-//            .placeholder(R.drawable.logointro)
-//            .error(R.drawable.logointro)
+            .placeholder(R.drawable.logo)
+            .error(R.drawable.logo)
             .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(32)))
             .diskCacheStrategy(DiskCacheStrategy.DATA)
     )
