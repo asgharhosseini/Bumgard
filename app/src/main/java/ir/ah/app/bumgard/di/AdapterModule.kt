5ac57e85cac437ba3079c13f6d47991b73a24e74
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-18 12:21:56 +0430

    add AdapterModule.kt

---
+++ app/src/main/java/ir/ah/app/bumgard/di/AdapterModule.kt
@@ -0,0 +1,18 @@
+package ir.ah.app.bumgard.di
+
+import com.bumptech.glide.*
+import dagger.*
+import dagger.hilt.*
+import dagger.hilt.android.components.*
+import ir.ah.app.bumgard.ui.search.adapter.*
+
+@Module
+@InstallIn(FragmentComponent::class)
+object AdapterModule {
+
+
+    @Provides
+    fun provideTopCityAdapter(glide: RequestManager) = TopCityAdapter(glide)
+    @Provides
+    fun providePopularCityAdapter(glide: RequestManager) = PopularCityAdapter(glide)
+}
