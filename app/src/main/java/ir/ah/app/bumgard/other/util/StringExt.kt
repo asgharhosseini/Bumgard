b7ce8b7f2c142269a6c71c0885e6f057fa228b37
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-20 13:26:49 +0430

    add  getTodayDate and  getTomorrowDate function in StringExt.kt

--- app/src/main/java/ir/ah/app/bumgard/other/util/StringExt.kt
+++ app/src/main/java/ir/ah/app/bumgard/other/util/StringExt.kt
@@ -16,3 +16,20 @@ fun dateFormatted(date: Long): String {
 
     return DateFormat.getDateInstance(DateFormat.MEDIUM).format(Date(date))
 }
+
+fun getTodayDate():String{
+    val dateFormat: DateFormat = SimpleDateFormat("dd-MMM-yyyy")
+    val calendar = Calendar.getInstance()
+    val today = calendar.time
+    return dateFormat.format(today)
+
+}
+fun getTomorrowDate():String{
+    val dateFormat: DateFormat = SimpleDateFormat("dd-MMM-yyyy")
+    val calendar = Calendar.getInstance()
+    calendar.add(Calendar.DAY_OF_YEAR, 1)
+    val tomorrow = calendar.time
+  return dateFormat.format(tomorrow)
+}
+
+
