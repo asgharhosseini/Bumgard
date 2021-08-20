bd2790e09adbd8738ca5acd6c13ab17ef785a9a2
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-20 13:22:14 +0430

    fix bug in name value HotelResponse.kt

--- app/src/main/java/ir/ah/app/bumgard/data/model/HotelResponse.kt
+++ app/src/main/java/ir/ah/app/bumgard/data/model/HotelResponse.kt
@@ -5,7 +5,7 @@ import com.squareup.moshi.*
 @JsonClass(generateAdapter = true)
 data class HotelResponse(
     @Json(name = "issue")
-    val cities: List<Hotel>,
+    val hotels: List<Hotel>,
     @Json(name = "totalPages")
     val totalPages: Int,
     @Json(name = "pageSize")
