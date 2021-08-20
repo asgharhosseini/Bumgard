3d6b1d7ecccd3b3728322a62f4301ca8275fe26c
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-20 15:47:38 +0430

    set new ui in SearchAdapter.kt

--- app/src/main/java/ir/ah/app/bumgard/ui/search/adapter/SearchAdapter.kt
+++ app/src/main/java/ir/ah/app/bumgard/ui/search/adapter/SearchAdapter.kt
@@ -7,6 +7,7 @@ import ir.ah.app.bumgard.data.model.*
 import ir.ah.app.bumgard.databinding.*
 import kotlinx.coroutines.flow.*
 import javax.inject.*
+import kotlin.random.*
 
 class SearchAdapter @Inject constructor(private val glide: RequestManager) :
     BaseAdapter<Hotel, SearchAdapter.SearchViewHolder>() {
@@ -18,6 +19,10 @@ class SearchAdapter @Inject constructor(private val glide: RequestManager) :
             binding.apply {
                 glide.load(hotel.image).into(imgItemSearch)
                 txtItemSearchTitle.text=hotel.name
+                Space.text="${hotel.spaceToCentrum} km to centrum"
+                price.text=hotel.price
+                rateReview.text="${hotel.rate} / ${hotel.reviews} reviews "
+                materialRatingBar.rating=hotel.rate.toFloat()
             }
         }
 
