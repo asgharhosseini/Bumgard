e301c6647999137a2ea6daf16c1f007d0b6defa0
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-21 19:38:44 +0430

    add FacilitiesAdapter selectable

--- app/src/main/java/ir/ah/app/bumgard/ui/search/filter/adapter/FacilitiesAdapter.kt
+++ app/src/main/java/ir/ah/app/bumgard/ui/search/filter/adapter/FacilitiesAdapter.kt
@@ -1,39 +1,69 @@
 package ir.ah.app.bumgard.ui.search.filter.adapter
 
+
+import android.annotation.*
+import android.graphics.*
+import android.graphics.drawable.*
 import android.view.*
+import androidx.recyclerview.selection.*
 import androidx.recyclerview.widget.*
-import com.bumptech.glide.*
+import ir.ah.app.bumgard.*
+import ir.ah.app.bumgard.R
+
 import ir.ah.app.bumgard.base.*
 import ir.ah.app.bumgard.data.model.*
 import ir.ah.app.bumgard.databinding.*
 import ir.ah.app.bumgard.ui.search.adapter.*
-import javax.inject.*
 
-class FacilitiesAdapter @Inject constructor(private val glide: RequestManager) :
+
+class FacilitiesAdapter :
     BaseAdapter<Facilities, FacilitiesAdapter.FacilitiesViewHolder>() {
-
+    init {
+        setHasStableIds(true)
+    }
+    var tracker: SelectionTracker<String>? = null
     inner class FacilitiesViewHolder(private val binding: ItemFacilitiesBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(facilities: Facilities) {
+
+        fun bind(facilities: Facilities, selected: Boolean) {
             binding.apply {
-                title.text=facilities.name
+                title.text = facilities.name
                 imageView.setImageResource(facilities.image)
+                if (selected){
+                    parent.setBackgroundResource(R.drawable.background_facilities_selected_color)
+
+                }else{
+                    parent.setBackgroundResource(R.drawable.background_facilities_deselected_color)
+                }
 
             }
         }
 
+        fun getItemDetails(): ItemDetailsLookup.ItemDetails<String> =
+            object : ItemDetailsLookup.ItemDetails<String>() {
+                override fun getPosition(): Int = adapterPosition
+                override fun getSelectionKey(): String? = getItem(position).name
+            }
+
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):FacilitiesViewHolder {
-        val binding = ItemFacilitiesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    override fun getItemId(position: Int): Long = position.toLong()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FacilitiesViewHolder {
+        val binding =
+            ItemFacilitiesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return FacilitiesViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: FacilitiesViewHolder, position: Int) {
         val currentItem = getItem(position)
-        holder.bind(currentItem)
+        //holder.bind(currentItem, it.isSelected(position.toLong()))
+
+        tracker?.let {
+            holder.bind(currentItem, it.isSelected(currentItem.name))
+        }
     }
+
 
 
 }
