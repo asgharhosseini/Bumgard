9709662d715194a299df156d7869fcb2acdc6465
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-23 14:42:23 +0430

    add getCity in CityViewModel.kt

--- app/src/main/java/ir/ah/app/bumgard/ui/city/CityViewModel.kt
+++ app/src/main/java/ir/ah/app/bumgard/ui/city/CityViewModel.kt
@@ -2,17 +2,36 @@ package ir.ah.app.bumgard.ui.city
 
 import dagger.hilt.android.lifecycle.*
 import ir.ah.app.bumgard.base.*
+import ir.ah.app.bumgard.data.model.*
 import ir.ah.app.bumgard.data.repository.city.*
+import ir.ah.app.bumgard.other.wrapper.*
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
 import kotlinx.coroutines.flow.*
 import javax.inject.*
 
 @HiltViewModel
 class CityViewModel @Inject constructor(
     val mainCoroutineDispatcher: CoroutineDispatcher,
-    repository: CityRepository
+    private val repository: CityRepository
 ) : BaseViewModel(mainCoroutineDispatcher) {
+    val cityName: MutableStateFlow<String> = MutableStateFlow("")
+    private val hotelInCityChanel = Channel<Resource<HotelResponse>>()
+    val hotelInCity = hotelInCityChanel.receiveAsFlow()
+
+    private fun getSearch() =
+        doInMain {
+            hotelInCityChanel.send(Resource.Loading)
+
+            cityName.value?.let {
+                hotelInCityChanel.send(
+                    repository.getCity(it)
+                )
+            }
+
+        }
+
 }
 
 
