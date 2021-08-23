f1b37001e90de345d28b64ef631b7728319cb4a8
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-23 14:32:23 +0430

    add DummyCityRepository.kt

---
+++ app/src/main/java/ir/ah/app/bumgard/data/repository/city/dummy/DummyCityRepository.kt
@@ -0,0 +1,16 @@
+package ir.ah.app.bumgard.data.repository.city.dummy
+
+import ir.ah.app.bumgard.data.model.*
+import ir.ah.app.bumgard.data.repository.city.*
+import ir.ah.app.bumgard.data.repository.search.*
+import ir.ah.app.bumgard.data.repository.factory.DummyFactory.DummyCityResponse
+import ir.ah.app.bumgard.data.repository.factory.DummyFactory.DummyFacilitiesResponse
+import ir.ah.app.bumgard.data.repository.factory.DummyFactory.DummyHotelResponse
+import ir.ah.app.bumgard.other.wrapper.*
+
+class DummyCityRepository :CityRepository {
+    override suspend fun getSearch(cityName: String): Resource<HotelResponse> =
+        Resource.Success(DummyHotelResponse)
+
+
+}
