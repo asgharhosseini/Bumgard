6000a521166a92d494a9d538988ec168796acecb
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-23 14:32:09 +0430

    add CityRepository.kt

---
+++ app/src/main/java/ir/ah/app/bumgard/data/repository/city/CityRepository.kt
@@ -0,0 +1,10 @@
+package ir.ah.app.bumgard.data.repository.city
+
+import ir.ah.app.bumgard.data.model.*
+import ir.ah.app.bumgard.other.wrapper.*
+
+interface CityRepository {
+
+    suspend fun getSearch(cityName: String): Resource<HotelResponse>
+
+}
