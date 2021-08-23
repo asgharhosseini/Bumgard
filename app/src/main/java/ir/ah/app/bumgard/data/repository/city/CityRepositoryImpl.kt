7dcb734f0d1cea38733ca9fb56fe177891ec8014
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-23 14:33:28 +0430

    add CityRepositoryImpl.kt

---
+++ app/src/main/java/ir/ah/app/bumgard/data/repository/city/CityRepositoryImpl.kt
@@ -0,0 +1,14 @@
+package ir.ah.app.bumgard.data.repository.city
+
+import ir.ah.app.bumgard.data.model.*
+import ir.ah.app.bumgard.data.remote.*
+import ir.ah.app.bumgard.other.util.*
+import ir.ah.app.bumgard.other.wrapper.*
+import javax.inject.*
+
+class CityRepositoryImpl @Inject constructor(private val apiService: ApiService)  :CityRepository {
+    override suspend fun getSearch(cityName: String): Resource<HotelResponse> =
+        safeApiCall { apiService.getSearch(cityName) }
+
+
+}
