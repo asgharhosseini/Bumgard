198bd5f3866969cf9fb992ef68f60e1ea5521ca2
asghar hosseini <asgharhosseini.v@gmail.com>
2021-08-27 16:32:03 +0430

    add HotelDetailViewModel.kt

--- app/src/main/java/ir/ah/app/bumgard/ui/hotel/HotelDetailViewModel.kt
+++ app/src/main/java/ir/ah/app/bumgard/ui/hotel/HotelDetailViewModel.kt
@@ -2,9 +2,12 @@ package ir.ah.app.bumgard.ui.hotel
 
 import dagger.hilt.android.lifecycle.*
 import ir.ah.app.bumgard.base.*
+import ir.ah.app.bumgard.data.model.*
 import ir.ah.app.bumgard.data.repository.hotel.*
+import ir.ah.app.bumgard.other.wrapper.*
 
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.*
 import kotlinx.coroutines.flow.*
 import javax.inject.*
 
@@ -13,6 +16,16 @@ class HotelDetailViewModel @Inject constructor(
     val mainCoroutineDispatcher: CoroutineDispatcher,
     private val hotelRepository: HotelRepository
 ) : BaseViewModel(mainCoroutineDispatcher) {
+    val hotelId: MutableStateFlow<Int> = MutableStateFlow(0)
+    private val hotelDetailChanel = Channel<Resource<HotelDetailResponse>>()
+    val hotelDetail = hotelDetailChanel.receiveAsFlow()
+
+    fun getHotelInCity() =
+        doInMain {
+            hotelDetailChanel.send(Resource.Loading)
+
+            hotelDetailChanel.send(hotelRepository.getHotelDetail(hotelId.value))
+            }
 }
 
 
